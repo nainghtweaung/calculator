05e44f70506b30eaacbdd908d0092b6634ccda46
nainghtweaung <114207394+nainghtweaung@users.noreply.github.com>
2024-01-27 15:00:09 +0630

    Implement consecutive operations

--- script.js
+++ script.js
@@ -14,20 +14,78 @@ function divide(a, b) {
   return Number(a) / Number(b);
 }
 
-let firstVariable, secondVariable, operator;
+let firstVariable = "";
+let secondVariable = "";
+let currentOperator = "";
+let currentValue = 0;
 
 function operate(num1, num2, operator) {
   switch (operator) {
     case "+":
-      add();
-      break;
+      return add(num1, num2);
     case "-":
-      subtract();
+      return subtract(num1, num2);
     case "*":
-      multiply();
-      break;
+      return multiply(num1, num2);
     case "/":
-      divide();
-      break;
+      return divide(num1, num2);
   }
 }
+
+const operators = document.querySelectorAll(".operator");
+const specialOperators = document.querySelectorAll(".special-operator");
+const numbers = document.querySelectorAll(".number");
+const displayValue = document.querySelector(".display");
+
+displayValue.textContent = currentValue;
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (firstVariable === "" && secondVariable === "") {
+      return;
+    }
+    // If currentOperator is not empty and equal was pressed calculate
+    console.log(operator.dataset.type);
+    if (currentOperator !== "") {
+      currentValue = operate(firstVariable, secondVariable, currentOperator);
+      displayValue.textContent = currentValue;
+      firstVariable = currentValue;
+      secondVariable = "";
+      currentOperator = "";
+      return;
+    }
+    if (currentOperator === "" && operator.dataset.type !== "=") {
+      currentOperator = operator.dataset.type;
+    }
+  });
+});
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    console.log(number.dataset.value);
+    if (firstVariable !== "" && currentOperator !== "") {
+      secondVariable += number.dataset.value;
+      displayValue.textContent = secondVariable;
+
+      return;
+    }
+    // Store value in first variable only if current operator is null
+    if (currentValue !== 0 && currentOperator === "") {
+      firstVariable = "";
+      currentValue = 0;
+    }
+    if (currentOperator === "") {
+      firstVariable += number.dataset.value;
+      displayValue.textContent = firstVariable;
+    } else {
+      secondVariable += number.dataset.value;
+      displayValue.textContent = secondVariable;
+    }
+  });
+});
+
+specialOperators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    console.log(operator.dataset.type);
+  });
+});
